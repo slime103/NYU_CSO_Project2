3b1addc3ebe32f60b69d605d23bde13cca5b7d33
Zach <44850453+slime103@users.noreply.github.com>
2020-10-07 12:36:31 -0400

    Assignment Files

---
+++ problem2/bst.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "bst.h"
+
+
+void add ( bst_node ** root, char * word ) {
+    
+}
+
+ 
+void inorder ( bst_node * root ) {
+    
+}
+
+
+ 
+char * removeSmallest (  bst_node ** root ){
+    
+    return NULL;
+}
+
+ 
+char * removeLargest (  bst_node ** root ){
+    
+    return NULL;
+}
+
+
+
+
---
+++ problem2/float.c
@@ -0,0 +1,50 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include "float.h"
+
+
+int is_special ( float f ) {
+
+    return 0;
+
+}
+
+
+float get_M  ( float f ) {
+    
+    return 0.0; 
+}
+
+
+int get_s ( float f ) {
+	
+	return 0;
+}
+
+
+
+int get_E ( float f ) {
+	
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+      
+
+
+
+    
+    
